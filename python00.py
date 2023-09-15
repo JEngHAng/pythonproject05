1998933b5f27afdba15bba1a565d4ebf229a9b5e
JEngHAng <jangg500z@gmail.com>
2023-09-15 09:57:14 +0700

    Code 01 : def

---
+++ python00.py
@@ -0,0 +1,7 @@
+#def parameter
+def funcA( x , y , z ):
+    print ( x + y + z)
+    print ("EIEI\n")
+
+funcA( 10 , 25 , 60 )
+funcA( 5 , 5 , 6)
---
+++ python01.py
@@ -0,0 +1,12 @@
+def funcA( ):
+    print ("AAA")
+    print ("BBB")
+    return #ตัวหยุด
+    print ("CCC")
+
+def funcB( x ):
+    return
+    print (f"X is {x}")
+
+funcA()
+funcB(25)
